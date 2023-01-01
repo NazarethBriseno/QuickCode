07e430328ffb1c2eb61e1990791e76333bb5b4e5
Nazareth-1 <nazarethbriseno@gmail.com>
2023-01-01 14:01:38 -0600

    Added stringCharacter Function

--- js/codewars.js
+++ js/codewars.js
@@ -1,11 +1,28 @@
-//Takes an array and adds a counter for each index
-function numbers(array){
-    let newArray = [];
-    array.forEach((element, index) => {
-        element = index + 1 + ": " + element;
-        newArray.push(element);
-    });
-    return newArray;
-}
-//Simpler version with .map
-let nugimbersWithMap = (a) => a.map((element, index) => `${index + 1}: ${element}`);
+// //Takes an array and adds a counter for each index
+// function numbersCounter(array){
+//     let newArray = [];
+//     array.forEach((element, index) => {
+//         element = index + 1 + ": " + element;
+//         newArray.push(element);
+//     });
+//     return newArray;
+// }
+// //Simpler version with .map
+// let numbersWithMap = (a) => a.map((element, index) => `${index + 1}: ${element}`);
+let array = [4, 5, 6, 7, 1];
+
+    function findSmallestInt(args) {
+        return Math.min(...args);
+    }
+
+       function findSmallestIntBySort(args) {
+            args.sort(function(a, b) {
+                return a - b; } )
+            return args[0];
+        }
+
+
+        let stringArray = ["Hello", "What", "No", "For sure!", "Yes", "Love it", "Course", "Library", "Bible", "Universe", "Earth", "King", "Easter", "Eggs", "Christmas", "New Years", "Water", "Farm", "Food", "Make"];
+        function getStringsWithCharacterCount(number, array){
+            return array.filter(element => element.length === number);
+        }
